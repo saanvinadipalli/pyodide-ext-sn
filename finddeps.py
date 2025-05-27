fae2a0a3a6ec903b05df7aa1882812b2650af2c1
Name <Email>
2025-05-27 13:10:45 -0700

    update finddeps

--- finddeps.py
+++ finddeps.py
@@ -7,7 +7,7 @@ from packaging.requirements import Requirement
 base_packages = set()
 pyodide_packages = open('pyodide-packages.txt').readlines()
 for p in pyodide_packages:
-    base_packages.add(p.strip().lower())
+    base_packages.add(p.strip())
 
 def get_package_deps(package_name):
     dependencies = set()
@@ -18,7 +18,7 @@ def get_package_deps(package_name):
         for dep in requires_dist:
             req = Requirement(dep)
             if not req.marker:
-                dependencies.add(req.name.lower())
+                dependencies.add(req.name)
     except Exception as e:
         pass
     return dependencies
@@ -31,17 +31,31 @@ def get_all_package_deps_tree(package_name, tree, base_packages):
         for dep in tree[package_name]:
             get_all_package_deps_tree(dep, tree, base_packages)
 
+def get_foundation_deps(package_name, tree, base_packages):
+    deps = get_package_deps(package_name)
+    tree['other_deps'] = deps.difference(base_packages)
+    tree['pyodide_deps'] = deps.intersection(base_packages)
+
 if __name__ == '__main__':
 
     import sys
     import pprint
 
-    if not len(sys.argv) == 2:
+    if not len(sys.argv) >= 2:
         print('Usage: ./finddeps.py <package name>')
         sys.exit(1)
-    root_package = sys.argv[1]
+    root_package = sys.argv[-1]
     tree = {}
-    get_all_package_deps_tree(root_package, tree, base_packages)
+    if len(sys.argv) == 2:
+        get_all_package_deps_tree(root_package, tree, base_packages)
+    else:
+        get_foundation_deps(root_package, tree, base_packages)
     pretty_print = pprint.PrettyPrinter()
     pretty_print.pprint(tree)
-
+    if len(sys.argv) != 2:
+        if tree['other_deps'] or tree['pyodide_deps']:
+            all_deps = tree['other_deps'].union(tree['pyodide_deps'])
+            print('requirements:')
+            print('  run:')
+            for dep in all_deps:
+                print(f'    - {dep}')
